8919f9c61f2f12475945354072961793c4d1fd6e
LEZ win <leeinsteinzhang@gmail>
2024-01-08 19:46:58 -0500

    update

--- memory.py
+++ memory.py
@@ -3,7 +3,22 @@ class Byte:
         self.data = [0, 0, 0, 0, 0, 0, 0, 0]
 
     def write(self, data):
-        pass
+        if len(data) == 8 and all(bit in [0, 1] for bit in data):
+            self.data = data
+            return True
+        else:
+            return False
 
     def read(self):
-        pass
+        return self.data
+
+
+class Memory:
+    def __init__(self):
+        self.size = 4 * 1024 * 1024 # 4MB memory
+        self.cells = [Byte() for i in range(self.size)]
+
+
+M = Memory()
+for i in range(4 * 1024 * 1024):
+    print(M.cells[i].data)
